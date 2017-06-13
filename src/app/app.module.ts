a72399be4c24327059f0d356d11cce9183405f82
Kashinath Kanki <kaskanki@publicisgroupe.net>
2017-06-13 17:37:49 +0530

    structure added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SearchComponent } from './search/search.component';
+import { CartComponent } from './cart/cart.component';
+import { ContentPartComponent } from './content-part/content-part.component';
+import { SortingComponent } from './sorting/sorting.component';
+import { ProductListingComponent } from './product-listing/product-listing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    SearchComponent,
+    CartComponent,
+    ContentPartComponent,
+    SortingComponent,
+    ProductListingComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/content-part/content-part.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-content-part',
+  templateUrl: './content-part.component.html',
+  styleUrls: ['./content-part.component.scss']
+})
+export class ContentPartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
