68a91004ff19a3d19abc09f8acdcdb7c1ba8996d
Kashinath Kanki <kaskanki@publicisgroupe.net>
2017-06-16 18:42:33 +0530

    further updates

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -5,11 +5,25 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
+
 export class NavbarComponent implements OnInit {
 
   constructor() { }
 
+  public happyText = "I m on vaction";
+  public myTodoList = [
+  	'Todo 1',
+  	'Todo 2',
+  	'Todo 3',
+  	'Todo 4 - fourth'
+  ];
+
   ngOnInit() {
+  	this.happyText = "I m sick not!!";
+  }
+
+  makeMeHappy(){
+  	this.happyText = "Manager is fired!!"
   }
 
 }
--- src/app/product-listing/product-listing.component.ts
+++ src/app/product-listing/product-listing.component.ts
@@ -6,10 +6,93 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-listing.component.scss']
 })
 export class ProductListingComponent implements OnInit {
-
+	
   constructor() { }
 
+  public proListing = [
+  	{
+  		proImg: 'https://assetscdn1.paytm.com/images/catalog/product/M/MO/MOBLENOVO-VIBE-A-TO2519725AF095B8/0x1920/70/0.jpeg',
+  		name: 'Lenovo Vibe K5',
+  		price: '12000',
+  		screenSize: 5.5
+  	},
+  	{
+  		proImg: 'https://assetscdn1.paytm.com/images/catalog/product/M/MO/MOBLENOVO-VIBE-A-TO2519725AF095B8/0x1920/70/0.jpeg',
+  		name: 'Abnovo Vibe K6',
+  		price: '10000',
+  		screenSize: 6.0,
+  		discount: {
+  			'd1': 10,
+  			'd2': 20
+  		}
+  	},
+  	{
+  		proImg: 'https://assetscdn1.paytm.com/images/catalog/product/M/MO/MOBLENOVO-VIBE-A-TO2519725AF095B8/0x1920/70/0.jpeg',
+  		name: 'Lenovo Vibe K7',
+  		price: '16000',
+  		screenSize: 4.5
+  	},
+  	{
+  		proImg: 'https://assetscdn1.paytm.com/images/catalog/product/M/MO/MOBLENOVO-VIBE-A-TO2519725AF095B8/0x1920/70/0.jpeg',
+  		name: 'RU Lenovo Vibe K8 - 123',
+  		price: '13000',
+  		screenSize: 5.75
+  	},
+  	{
+  		proImg: 'https://assetscdn1.paytm.com/images/catalog/product/M/MO/MOBLENOVO-VIBE-A-TO2519725AF095B8/0x1920/70/0.jpeg',
+  		name: 'ZE Lenovo Vibe K9',
+  		price: '18000',
+  		screenSize: 4.5,
+  		discount: {
+  			'd1': 10,
+  			'd2': 20
+  		}
+  	},
+  	{
+  		proImg: 'https://assetscdn1.paytm.com/images/catalog/product/M/MO/MOBLENOVO-VIBE-A-TO2519725AF095B8/0x1920/70/0.jpeg',
+  		name: 'Lenovo Vibe K10',
+  		price: '20000',
+  		screenSize: 5.75
+  	},
+  	{
+  		proImg: 'https://assetscdn1.paytm.com/images/catalog/product/M/MO/MOBLENOVO-VIBE-A-TO2519725AF095B8/0x1920/70/0.jpeg',
+  		name: 'DcLenovo Vibe K11',
+  		price: '23000',
+  		screenSize: 5.75,
+  		discount: {
+  			'd1': 10,
+  			'd2': 20
+  		}
+  	},
+  	{
+  		proImg: 'BBhttps://assetscdn1.paytm.com/images/catalog/product/M/MO/MOBLENOVO-VIBE-A-TO2519725AF095B8/0x1920/70/0.jpeg',
+  		name: 'Lenovo Vibe K12',
+  		price: '24000',
+  		screenSize: 5.75
+  	},
+  	{
+  		proImg: 'https://assetscdn1.paytm.com/images/catalog/product/M/MO/MOBLENOVO-VIBE-A-TO2519725AF095B8/0x1920/70/0.jpeg',
+  		name: 'ABLenovo Vibe K13',
+  		price: '28000',
+  		screenSize: 5.75
+  	},
+  	{
+  		proImg: 'https://assetscdn1.paytm.com/images/catalog/product/M/MO/MOBLENOVO-VIBE-A-TO2519725AF095B8/0x1920/70/0.jpeg',
+  		name: 'OpLenovo Vibe K13',
+  		price: '32000',
+  		screenSize: 5.75
+  	},
+  	{
+  		proImg: 'https://assetscdn1.paytm.com/images/catalog/product/M/MO/MOBLENOVO-VIBE-A-TO2519725AF095B8/0x1920/70/0.jpeg',
+  		name: 'KLenovo Vibe K13',
+  		price: '56000',
+  		screenSize: 5.75
+  	}
+  ];
+
+
   ngOnInit() {
+
   }
 
 }
